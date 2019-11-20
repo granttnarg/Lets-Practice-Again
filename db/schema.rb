56afea9abab23980a31812d95b19ded0f78c9319
grant thomas <grantwilliamthomas@hotmail.co.uk>
2019-11-20 16:20:08 +0100

    added code and show files for song show, index and new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_142956) do
+ActiveRecord::Schema.define(version: 2019_11_20_151603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_142956) do
     t.string "time_signature"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   add_foreign_key "notes", "songs"

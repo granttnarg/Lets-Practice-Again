10fa731ed1c6b318c8f8ada830f7d88519c12cc6
grant thomas <grantwilliamthomas@hotmail.co.uk>
2019-11-20 15:39:18 +0100

    install cloudinary and carier wave attached to song model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :song
-end
+#   validates :date, presence: true
+#   validates :description, presence: true
+#   validates :prac_bpm, presence: true
+#   validates :bar_progress, presence: true
+#   validates :rating, presence: true, :inclusion => { :in => 1..5 }
+# end

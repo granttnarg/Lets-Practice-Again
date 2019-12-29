e8acf3f3b37499ae2f772cd42b0f3f39c8bf9ba9
grant thomas <grantwilliamthomas@hotmail.co.uk>
2019-12-29 19:38:22 +0100

    done

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Song, type: :model do
   before(:all) do
-    @song = Song.new(name: "Sandstorm", artist: "Darude", key: "11", native_bpm: "111", photo: "image/upload/v1574263004/e2ob8sbkf2wsw2m7onjd.jpg")
+    @song = Song.new(name: "Sandstorm", artist: "Darude", key: "11", native_bpm: "111", photo: "image/upload/v1574263004/e2ob8sbkf2wsw2m7onjd.jpg", info: "A song", chords: "E - Em - Dm - Am - A - D - Bm - B - E7 - Bm7- Em7",
+  time_signature: "4/4",)
   end
 
   it "should have a track name" do
@@ -11,6 +12,18 @@ RSpec.describe Song, type: :model do
 
   it "should have a artist name" do
     expect(@song.artist).to eq("Darude")
+  end
+
+   it "should have song info" do
+    expect(@song.info).to eq("A song")
+  end
+
+   it "should have chords" do
+    expect(@song.chords).to eq("E - Em - Dm - Am - A - D - Bm - B - E7 - Bm7- Em7")
+  end
+
+   it "should have a time time_signature" do
+    expect(@song.time_signature).to eq("4/4")
   end
 
   it "should have a key" do

0be164e2eb381ff8b6d5a06be06880969eb7d037
grant thomas <grantwilliamthomas@hotmail.co.uk>
2019-12-29 19:34:45 +0100

    tested model song

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @song = Song.new(name: "Sandstorm", artist: "Darude", key: "11", native_bpm: "111", photo: "image/upload/v1574263004/e2ob8sbkf2wsw2m7onjd.jpg")
+  end
+
+  it "should have a track name" do
+    expect(@song.name).to eq("Sandstorm")
+  end
+
+  it "should have a artist name" do
+    expect(@song.artist).to eq("Darude")
+  end
+
+  it "should have a key" do
+    expect(@song.key).to eq("11")
+  end
+
+  it "should have a photo saved with PhotoUploader" do
+    expect(@song.photo).to be_a PhotoUploader
+  end
+
+  it "should have a native_bpm" do
+    expect(@song.native_bpm).to eq(111)
+    expect(@song.native_bpm).to be_a Integer
+  end
 end
